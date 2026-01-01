a22e1c7435e79c3d8e0612aa6354157cd3d8d185
Kannat-samy <didinesamy42@gmail.com>
2026-01-01 16:47:31 +0000

    create my world gazebo (myturtlebot3_world)

--- src/tb3_multi_bringup/launch/tb3_simulation_launch.py
+++ src/tb3_multi_bringup/launch/tb3_simulation_launch.py
@@ -90,7 +90,7 @@ def generate_launch_description():
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
         default_value=os.path.join(
-            this_pkg_dir, 'maps', 'map_stage4.yaml'),
+            this_pkg_dir, 'maps', 'turtlebot3_world.yaml'),
         description='Full path to map file to load')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
@@ -148,7 +148,7 @@ def generate_launch_description():
         default_value=os.path.join(
             turtlebot3_gazebo_dir,
             'worlds',
-            'turtlebot3_dqn_stage4.world'), 
+            'turtlebot3_world.world'), 
         description='Full path to world model file to load')
 
     #declare_world_cmd = DeclareLaunchArgument(
